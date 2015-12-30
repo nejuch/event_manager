282240c9bc408c4036b2166e378ae22f5d550982
m11t <melfthomsen@gmail.com>
2015-12-30 13:28:24 +0100

    bramksi/angular-indexedDB as indexedDB-Wrapper

--- js/event-manager.js
+++ js/event-manager.js
@@ -3,8 +3,29 @@
  *
  */
 (function() {
-  var eventManagerApp = angular.module('eventManagerApp', ['eventManagerDB']);
+  var app = angular.module('eventManager', ['indexedDB']);
+
+  /**
+   * IndexedDB-Configuration
+   */
+  app.config(function($indexedDBProvider) {
+    var db = $indexedDBProvider.connection('EventManagerDB');
+
+    // ~~~ Create initial database version
+    db.upgradeDatabase(1, function(pEvent, pDatabase, pTransaction) {
+      pDatabase.createObjectStore('Event', {keyPath: 'person_id', autoIncrement: true});
+      pDatabase.createObjectStore('Event_Equip', {keyPath: ['person_id', 'equipment_id']});
+      pDatabase.createObjectStore('Equipment', {keyPath: 'equipment_id'});
+      pDatabase.createObjectStore('Location', {keyPath: 'location_id'});
+      pDatabase.createObjectStore('Tracklist', {keyPath: 'tracklist_id'});
+      pDatabase.createObjectStore('Tracklist_Track', {keyPath: ['tracklist_id','track_id']});
+      pDatabase.createObjectStore('Track', {keyPath: 'track_id'});
+    });
+  });
 
 
+  app.controller('EventController', function() {
+    this.event = '';
+  });
 
-});
+})();
