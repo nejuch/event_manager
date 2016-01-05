502fd0cdc2f43af1b0ebb9cc13ca7154fea001af
m11t <melfthomsen@gmail.com>
2016-01-05 20:59:38 +0100

    Fixed Database upgrade ultimately ... hopefully

--- js/event-manager.js
+++ js/event-manager.js
@@ -15,14 +15,6 @@
     db.upgradeDatabase(1, function(pEvent, pDatabase, pTransaction) {
       pDatabase.createObjectStore('Event', {keyPath: 'person_id', autoIncrement: true});
       pDatabase.createObjectStore('Event_Equip', {keyPath: ['person_id', 'equipment_id']});
-      pDatabase.createObjectStore('Equipment', {keyPath: 'equipment_id'});
-      pDatabase.createObjectStore('Location', {keyPath: 'location_id'});
-      pDatabase.createObjectStore('Tracklist', {keyPath: 'tracklist_id'});
-      pDatabase.createObjectStore('Tracklist_Track', {keyPath: ['tracklist_id','track_id']});
-      pDatabase.createObjectStore('Track', {keyPath: 'track_id'});
-    }).upgradeDatabase(2, function(pEvent, pDatabase, pTransaction) {
-      pDatabase.createObjectStore('Event', {keyPath: 'person_id', autoIncrement: true});
-      pDatabase.createObjectStore('Event_Equip', {keyPath: ['person_id', 'equipment_id']});
       pDatabase.createObjectStore('Equipment', {keyPath: 'equipment_id', autoIncrement: true});
       pDatabase.createObjectStore('Location', {keyPath: 'location_id', autoIncrement: true});
       pDatabase.createObjectStore('Tracklist', {keyPath: 'tracklist_id', autoIncrement: true});
